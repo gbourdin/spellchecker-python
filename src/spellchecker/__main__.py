7e8347b2b1ed821c0a9bc3f1884cda1b0dbee08d
gbourdin <german.bourdin@gmail.com>
2015-05-05 01:43:24 -0300

    Empty classes for document and dictionary

--- src/spellchecker/__main__.py
+++ src/spellchecker/__main__.py
@@ -1,9 +1,13 @@
-from __future__ import unicode_literals
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import
 import sys
+
+from .dictionary import Dictionary
+from .document import Document
+
 
 def main():
     print("Oh noes, I'm not implemented yet!")
 
 if __name__ == "__main__":
     sys.exit(main())
-
